b63a8ab20556f7c60831ed5cacb5e2cb537213c7
Vikas <vikassingheng@gmail.com>
2018-11-02 15:44:18 -0500

    gauge initial

--- Plotly/Belly_Button_Biodiversity/static/js/app.js
+++ Plotly/Belly_Button_Biodiversity/static/js/app.js
@@ -1,16 +1,86 @@
+
+function buildGauge(frequency){
+
+var level = frequency;
+
+// Trig to calc meter point
+var degrees = 10 - level,
+     radius = .5;
+var radians = degrees * Math.PI / 180;
+var x = radius * Math.cos(radians);
+var y = radius * Math.sin(radians);
+
+// Path: may have to change to create a better triangle
+var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+     pathX = String(x),
+     space = ' ',
+     pathY = String(y),
+     pathEnd = ' Z';
+var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+var data = [{ type: 'scatter',
+   x: [0], y:[0],
+    marker: {size: 28, color:'850000'},
+    showlegend: false,
+    name: 'frequency',
+    text: level,
+    hoverinfo: 'text+name'},
+  { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9,50/9,50/9,50/9, 50],
+  rotation: 90,
+  text: ['0-1', '1-2', '2-3', '3-4',
+            '4-5', '5-6','6-7','7-8','8-9', ''],
+  textinfo: 'text',
+  textposition:'inside',
+  marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+                         'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+                         'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)','rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+                         'rgba(170, 202, 42, .5)',
+                         'rgba(255, 255, 255, 0)']},
+  labels: ['0-1', '1-2', '2-3', '3-4',
+  '4-5', '5-6','6-7','7-8','8-9', ''],
+  hoverinfo: 'label',
+  hole: .5,
+  type: 'pie',
+  showlegend: false
+}];
+
+var layout = {
+  shapes:[{
+      type: 'path',
+      path: path,
+      fillcolor: '850000',
+      line: {
+        color: '850000'
+      }
+    }],
+  title: 'Frequency 0-10',
+  height: 1000,
+  width: 1000,
+  xaxis: {zeroline:false, showticklabels:false,
+             showgrid: false, range: [-1, 1]},
+  yaxis: {zeroline:false, showticklabels:false,
+             showgrid: false, range: [-1, 1]}
+};
+
+Plotly.newPlot('gauge', data, layout);
+
+}
+
+
 function buildMetadata(sample) {
 
     // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
     d3.json(`/metadata/${sample}`).then((data) => {
-      d3.select("#sample-metadata").html("")
+      d3.select("#sample-metadata").html("");
 
       Object.entries(data).forEach(([k,v]) => {
         console.log(k + ":" + v);
         d3.select("#sample-metadata")
           .append("p").style("text-size:","6px").text(k + ":" + v)
           .append("br")
-      })
+      });
+      buildGauge(data.WFREQ);
+
     });
 }
 
